768329f4ad37796ddb9aa373c8fdcae3bd19566e
Hec <24020619@vnu.edu.vn>
2025-02-25 10:56:15 +0700

    Add files via upload

---
+++ 24020619_Lect2_Assignments/2.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+    node(int data)
+    {
+        this->data = data;
+        this->next = nullptr;
+    }
+};
+
+void insert(node *head, int p, int x)
+{
+    node *newNode = new node(x);
+    node *temp = head;
+    for (int i = 0; i < p - 1; i++)
+    {
+        temp = temp->next;
+    }
+    newNode->next = temp->next;
+    temp->next = newNode;
+}
+
+void delete_node(node *head, int p)
+{
+    node *temp = head;
+    for (int i = 0; i < p - 1; i++)
+    {
+        temp = temp->next;
+    }
+    node *temp2 = temp->next;
+    temp->next = temp2->next;
+    delete temp2;
+}
+
+int main()
+{
+    int ops;
+    cin >> ops;
+    cin.ignore();
+    node* head = nullptr;
+    for (int i = 0; i <= ops; i++)
+    {
+        string line;
+        getline(cin, line);
+        stringstream ss(line);
+        string op;
+        ss >> op;
+        if (op == "insert")
+        {
+            int p, x;
+            ss >> p >> x;
+            if (head == nullptr)
+            {
+                head = new node(x);
+            }
+            else
+            {
+                insert(head, p, x);
+            }
+        }
+        else if (op == "delete")
+        {
+            int p;
+            ss >> p;
+            delete_node(head, p);
+        }
+    }
+    while (head != nullptr)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+}
