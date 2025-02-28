2ea2a2c9bbeab6a57d5ec4dd220c32a527f4a95c
24020619 <24020619@vnu.edu.vn>
2025-02-28 23:42:53 +0700

    Update 1.cpp

--- 24020619_Lect2_Assignments/1.cpp
+++ 24020619_Lect2_Assignments/1.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
 int main(){
     int n; cin >> n;
-    vector<int> v(n);
+    int* v = new int[n];
     for (int i = 0; i < n; i++){
         cin >> v[i];
     }
